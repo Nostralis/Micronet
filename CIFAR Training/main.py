0d5b7691a4b52e29f409aed1145458f6cccaf8b3
Blu3s1one <74374450+Blu3s1one@users.noreply.github.com>
2021-03-18 13:08:37 +0100

    changed for [1,1,1] and longer training

--- CIFAR Training/main.py
+++ CIFAR Training/main.py
@@ -8,7 +8,7 @@ import time
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device ' + str(device))
 
-student_ = [2, 2, 2]
+student_ = [1, 1, 1]
 teacher_ = [2, 2, 2, 2]
 
 student = ResNet18(student_)
@@ -29,8 +29,8 @@ def teacher_loss(output, label):
 
 optimizer = torch.optim.SGD(student.parameters(), 0.1, weight_decay=0.0005)
 
-training_distillation(50, train_loader, valid_loader, student, teacher, criterion, teacher_loss, optimizer, 0.1, device,
-                      milestone=[20, 25])
+training_distillation(150, train_loader, valid_loader, student, teacher, criterion, teacher_loss, optimizer, 0.1, device,
+                      milestone=[80, 130])
 
 evaluation_distillation(student, teacher, test_loader, criterion, teacher_loss, device)
 
