e5ebdd0cee52f0c8d88d66b0f91a551d719ad6af
Blu3s1one <74374450+Blu3s1one@users.noreply.github.com>
2021-03-18 11:14:09 +0100

    YES

--- CIFAR Training/main.py
+++ CIFAR Training/main.py
@@ -8,159 +8,30 @@ import time
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device ' + str(device))
 
-list_models = [[2,2,2]]
-print("les modèles prunés sont: " + str(list_models))
+student_ = [2, 2, 2]
+teacher_ = [2, 2, 2, 2]
 
-courbe = []
-size = []
-miles = [5,8,15]
-for i in list_models:
-    print("modèle: " + str(i))
-    PATH = "model_trained" + str(i) + ".pth"
-    #PATH = str(i) + ".pth"
-    model = torch.load(PATH)
-    model.to(device=device)
-    optimizer = torch.optim.SGD(model.parameters(), 0.1, weight_decay=0.0005)
-    criterion = nn.CrossEntropyLoss()
-    training(100, train_loader, valid_loader, model, criterion, optimizer, 0.1, device, milestones = [50,80])
-    torch.save(model, "model_trained" + str(i) + ".pth")
-    dim = 0
-    lim = 0
-    courbe += [evaluation(model, test_loader, criterion, device)]
-    size += [print_nonzeros(model)]
-    for j in range(15):
-        if j == miles[lim]:
-            lim += 1
-        print("étape numéro " + str(j) + " du prunage")
-        pruner(model, dim, lim, 0.1)
-        dim += 1
-        dim %= 2
-        optimizer = torch.optim.SGD(model.parameters(), 0.1, weight_decay=0.0005)
-        training(40, train_loader, valid_loader, model, criterion, optimizer, 0.1, device)
-        courbe += [evaluation(model, test_loader, criterion, device)]
-        size += [print_nonzeros(model)]
-    #clean(model)
-    optimizer = torch.optim.SGD(model.parameters(), 0.01, weight_decay=0.0005)
-    training(80, train_loader, valid_loader, model, criterion, optimizer, 0.1, device)
-    evaluation(model, test_loader, criterion, device)
-    torch.save(model, "model_more_structured" + str(i) + ".pth")
+student = ResNet18(student_)
+teacher = ResNet18(teacher_)
+model = torch.load(str(teacher_) + ".pth")
+teacher.load_state_dict(model)
 
-print(courbe)
-print(size)
+student.to(device)
+teacher.to(device)
 
-""""
-PATH = '[1,1,1,1]/model[1,1,1,1].pth'
-checkpoint = torch.load(PATH)
-model.load_state_dict(checkpoint)
-model.to(device=device)
+print(student)
+print(teacher)
+
 criterion = nn.CrossEntropyLoss()
-courbe = []
-size = []
-courbe += [evaluation(model, test_loader, criterion, device)]
-print_nonzeros(model)
-size += [print_nonzeros(model)]
-dim = 0
-for name, module in model.named_modules():
-        # prune 20% of connections in all 2D-conv layers
-    if isinstance(module, torch.nn.Conv2d):
-            prune.ln_structured(module, name='weight', amount=0.65, n=2, dim=dim)
-dim += 1
-dim %= 2
-optimizer = torch.optim.SGD(model.parameters(), 0.1, weight_decay=0.0005)
-training(100, train_loader, valid_loader, model, criterion, optimizer, 0.1, device)
-courbe += [evaluation(model, test_loader, criterion, device)]
-print_nonzeros(model)
-size += [print_nonzeros(model)]
 
-clean(model)
-evaluation(model, test_loader, criterion, device)
-torch.save(model.state_dict(), 'modeldirect70%[1,1,1,1].pth')
 
-model.load_state_dict(checkpoint)
-model.to(device=device)
-criterion = nn.CrossEntropyLoss()
-courbe = []
-size = []
-courbe += [evaluation(model, test_loader, criterion, device)]
-print_nonzeros(model)
-size += [print_nonzeros(model)]
-dim = 0
-for i in range(3):
-    for name, module in model.named_modules():
-        # prune 20% of connections in all 2D-conv layers
-        if isinstance(module, torch.nn.Conv2d):
-            prune.ln_structured(module, name='weight', amount=0.3, n=2, dim=dim)
-    dim += 1
-    dim %= 2
-    optimizer = torch.optim.SGD(model.parameters(), 0.1)
-    courbe += [evaluation(model, test_loader, criterion, device)]
-    print_nonzeros(model)
-    size += [print_nonzeros(model)]
+def teacher_loss(output, label):
+    return torch.mean((output - label) ** 2)
 
-optimizer = torch.optim.SGD(model.parameters(), 0.1, weight_decay=0.0005)
-training(100, train_loader, valid_loader, model, criterion, optimizer, 0.1, device)
-clean(model)
-evaluation(model, test_loader, criterion, device)
-torch.save(model.state_dict(), 'modeletapenotrain70%[1,1,1,1].pth')
 
-model.load_state_dict(checkpoint)
-model.to(device=device)
-criterion = nn.CrossEntropyLoss()
-courbe = []
-size = []
-courbe += [evaluation(model, test_loader, criterion, device)]
-print_nonzeros(model)
-size += [print_nonzeros(model)]
-dim = 0
-for i in range(3):
-    for name, module in model.named_modules():
-        # prune 20% of connections in all 2D-conv layers
-        if isinstance(module, torch.nn.Conv2d):
-            prune.ln_structured(module, name='weight', amount=0.3, n=2, dim=dim)
-    dim += 1
-    dim %= 2
-    optimizer = torch.optim.SGD(model.parameters(), 0.1, weight_decay=0.0005)
-    training(100, train_loader, valid_loader, model, criterion, optimizer, 0.1, device)
-    courbe += [evaluation(model, test_loader, criterion, device)]
-    print_nonzeros(model)
-    size += [print_nonzeros(model)]
+optimizer = torch.optim.SGD(student.parameters(), 0.1, weight_decay=0.0005)
 
-clean(model)
-evaluation(model, test_loader, criterion, device)
-torch.save(model.state_dict(), 'modeletapetrain70%[1,1,1,1].pth')
+training_distillation(50, train_loader, valid_loader, student, teacher, criterion, teacher_loss, optimizer, 0.1, device,
+                      milestone=[20, 25])
 
-model.load_state_dict(checkpoint)
-model.to(device=device)
-criterion = nn.CrossEntropyLoss()
-courbe = []
-size = []
-courbe += [evaluation(model, test_loader, criterion, device)]
-print_nonzeros(model)
-size += [print_nonzeros(model)]
-dim = 0
-for i in range(5):
-    for name, module in model.named_modules():
-        # prune 20% of connections in all 2D-conv layers
-        if isinstance(module, torch.nn.Conv2d):
-            prune.ln_structured(module, name='weight', amount=0.2, n=2, dim=dim)
-    dim += 1
-    dim %= 2
-    optimizer = torch.optim.SGD(model.parameters(), 0.1, weight_decay=0.0005)
-    training(100, train_loader, valid_loader, model, criterion, optimizer, 0.1, device)
-    courbe += [evaluation(model, test_loader, criterion, device)]
-    print_nonzeros(model)
-    size += [print_nonzeros(model)]
-
-clean(model)
-evaluation(model, test_loader, criterion, device)
-torch.save(model.state_dict(), 'modelpetiteetapetrain70%[1,1,1,1].pth')
-
-print(courbe)
-print(size)
-plt.figure()
-plt.ylabel('Accuracy')
-plt.plot(size,courbe)
-plt.xlabel('% of parameters pruned')
-plt.show()
-
-"""
+evaluation_distillation(student, teacher, test_loader, criterion, teacher_loss, device)
--- CIFAR Training/trainer.py
+++ CIFAR Training/trainer.py
@@ -7,12 +7,13 @@ from torch.utils.data.dataloader import DataLoader
 import time
 
 
-def training(n_epochs, train_loader, valid_loader, model, criterion, optimizer, factor, device, bina=True, valid_loss_min = np.Inf, milestone = [25,35]):  # FUNCTION TO BE COMPLETED
+def training(n_epochs, train_loader, valid_loader, model, criterion, optimizer, factor, device, bina=True,
+             valid_loss_min=np.Inf, milestone=[25, 35]):  # FUNCTION TO BE COMPLETED
 
     lamda = factor
     train_losses, valid_losses = [], []
     torch.autograd.set_detect_anomaly(True)
-    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, verbose=True, milestones= milestone, gamma = 0.1)
+    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, verbose=True, milestones=milestone, gamma=0.1)
 
     if bina:
         for epoch in range(n_epochs):
@@ -96,33 +97,35 @@ def training(n_epochs, train_loader, valid_loader, model, criterion, optimizer, 
 
     return train_losses, valid_losses, valid_loss_min
 
+
 def evaluation(model, test_loader, criterion, device):
+    test_loss = 0.0
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
 
-  test_loss = 0.0
-  class_correct = list(0. for i in range(10))
-  class_total = list(0. for i in range(10))
+    model.eval()
+    for data, label in test_loader:
+        data = data.to(device=device, dtype=torch.float32)
+        label = label.to(device=device, dtype=torch.long)
+        with torch.no_grad():
+            output = model(data)
+        loss = criterion(output, label)
+        test_loss += loss.item() * data.size(0)
+        _, pred = torch.max(output, 1)
+        correct = np.squeeze(pred.eq(label.data.view_as(pred)))
+        for i in range(len(label)):
+            digit = label.data[i]
+            class_correct[digit] += correct[i].item()
+            class_total[digit] += 1
 
-  model.eval()
-  for data, label in test_loader:
-      data = data.to(device=device, dtype=torch.float32)
-      label = label.to(device=device, dtype=torch.long)
-      with torch.no_grad():
-          output = model(data)
-      loss = criterion(output, label)
-      test_loss += loss.item()*data.size(0)
-      _, pred = torch.max(output, 1)
-      correct = np.squeeze(pred.eq(label.data.view_as(pred)))
-      for i in range(len(label)):
-          digit = label.data[i]
-          class_correct[digit] += correct[i].item()
-          class_total[digit] += 1
-
-  test_loss = test_loss/len(test_loader.sampler)
-  print('test Loss: {:.6f}\n'.format(test_loss))
-  for i in range(10)                                                                                       :
-    print('test accuracy of %d: %2d%% (%2d/%2d)' % (i, 100 * class_correct[i] / class_total[i], np.sum(class_correct[i]), np.sum(class_total[i])))
-  print('\ntest accuracy (overall): %2.2f%% (%2d/%2d)' % (100. * np.sum(class_correct) / np.sum(class_total), np.sum(class_correct), np.sum(class_total)))
-  return (100. * np.sum(class_correct) / np.sum(class_total))
+    test_loss = test_loss / len(test_loader.sampler)
+    print('test Loss: {:.6f}\n'.format(test_loss))
+    for i in range(10):
+        print('test accuracy of %d: %2d%% (%2d/%2d)' % (
+        i, 100 * class_correct[i] / class_total[i], np.sum(class_correct[i]), np.sum(class_total[i])))
+    print('\ntest accuracy (overall): %2.2f%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total), np.sum(class_correct), np.sum(class_total)))
+    return (100. * np.sum(class_correct) / np.sum(class_total))
 
 
 def plot_loss(test_loss, training_loss, n_epochs):
@@ -218,13 +221,12 @@ def lr_trainings(learning_rates, epochs, pre_trained=False):
     return total_losses
 
 
-def create_model(lr, epoch, bina = False):
+def create_model(lr, epoch, bina=False):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device ' + str(device))
 
-
     model_1 = models.resnet18(pretrained=True)
-    model_1.fc=nn.Linear(512, 4)
+    model_1.fc = nn.Linear(512, 4)
     model_1.to(device=device)
 
     if bina:
@@ -236,18 +238,145 @@ def create_model(lr, epoch, bina = False):
     validloader = DataLoader(minicifar_train_im, batch_size=32, sampler=valid_sampler)
     testloader = DataLoader(minicifar_test_im, batch_size=32)
 
-
     optimizer = torch.optim.SGD(model_1.parameters(), lr)
-
 
     start_time = time.time()
 
-    train_losses_1, valid_losses_1 = training(epoch, trainloader, validloader, model_1, criterion, optimizer, 0.1, device)
+    train_losses_1, valid_losses_1 = training(epoch, trainloader, validloader, model_1, criterion, optimizer, 0.1,
+                                              device)
 
     print("--- %s seconds ---" % (time.time() - start_time))
-    t = (time.time() - start_time)/epoch*10
+    t = (time.time() - start_time) / epoch * 10
     PATH = 'data/model.pth'
     torch.save(model_1.state_dict(), PATH)
 
     evaluation(model_1, testloader, criterion, device)
-    return model_1,t
+    return model_1, t
+
+
+def training_distillation(n_epochs, train_loader, valid_loader, model, teacher, criterion1, criterion2, optimizer,
+                          factor, device, bina=True, valid_loss_min=np.Inf,
+                          milestone=[25, 35]):  # FUNCTION TO BE COMPLETED
+
+    lamda = factor
+    train_losses, valid_losses = [], []
+    torch.autograd.set_detect_anomaly(True)
+    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, verbose=True, milestones=milestone, gamma=0.1)
+
+    if bina:
+        for epoch in range(n_epochs):
+            train_loss, valid_loss = 0, 0
+
+            model.train()
+            for data, label in train_loader:
+                data = data.to(device=device, dtype=torch.float32)
+                label = label.to(device=device, dtype=torch.long)
+                optimizer.zero_grad()  # clear the gradients of all optimized variables
+                output = model(data)  # forward pass: compute predicted outputs by passing inputs to the model
+                output_teacher = teacher(data)
+                # loss = criterion1(output, label) + criterion2(output, output_teacher)  # calculate the loss
+                print(output)
+                print(output_teacher)
+                loss = criterion2(output, output_teacher)
+                #loss = criterion1(output, label)
+                loss.backward()  # backward pass: compute gradient of the loss with respect to model parameters
+                optimizer.step()  # perform a single optimization step (parameter update)
+                train_loss += loss.item() * data.size(0)  # update running training loss
+
+            model.eval()
+            for data, label in valid_loader:
+                data = data.to(device=device, dtype=torch.float32)
+                label = label.to(device=device, dtype=torch.long)
+                with torch.no_grad():
+                    output = model(data)
+                loss = criterion1(output, label)
+                valid_loss += loss.item() * data.size(0)
+
+            train_loss /= len(train_loader.sampler)
+            valid_loss /= len(valid_loader.sampler)
+            train_losses.append(train_loss)
+            valid_losses.append(valid_loss)
+            lr_scheduler.step()
+            print(
+                'epoch: {} \ttraining Loss: {:.6f} \tvalidation Loss: {:.6f}'.format(epoch + 1, train_loss, valid_loss))
+
+            if valid_loss <= valid_loss_min:
+                print('validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+                    valid_loss_min,
+                    valid_loss))
+                torch.save(model.state_dict(), 'model.pt')
+                valid_loss_min = valid_loss
+    else:
+        for epoch in range(n_epochs):
+            train_loss, valid_loss = 0, 0
+
+            model.model.train()
+            for data, label in train_loader:
+                model.binarization()
+                data = data.to(device=device, dtype=torch.float32)
+                label = label.to(device=device, dtype=torch.long)
+                optimizer.zero_grad()  # clear the gradients of all optimized variables
+                output = model.forward(data)  # forward pass: compute predicted outputs by passing inputs to the model
+                loss = criterion(output, label)  # calculate the loss
+                loss.backward()  # backward pass: compute gradient of the loss with respect to model parameters
+                model.restore()
+                optimizer.step()  # perform a single optimization step (parameter update)
+                model.clip()
+                train_loss += loss.item() * data.size(0)  # update running training loss
+
+            model.model.eval()
+            for data, label in valid_loader:
+                data = data.to(device=device, dtype=torch.float32)
+                label = label.to(device=device, dtype=torch.long)
+                with torch.no_grad():
+                    output = model.model(data)
+                loss = criterion(output, label)
+                valid_loss += loss.item() * data.size(0)
+
+            train_loss /= len(train_loader.sampler)
+            valid_loss /= len(valid_loader.sampler)
+            train_losses.append(train_loss)
+            valid_losses.append(valid_loss)
+            lr_scheduler.step(valid_loss)
+            print(
+                'epoch: {} \ttraining Loss: {:.6f} \tvalidation Loss: {:.6f}'.format(epoch + 1, train_loss, valid_loss))
+
+            if valid_loss <= valid_loss_min:
+                print('validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+                    valid_loss_min,
+                    valid_loss))
+                torch.save(model.model.state_dict(), 'model.pt')
+                valid_loss_min = valid_loss
+
+    return train_losses, valid_losses, valid_loss_min
+
+
+def evaluation_distillation(model, teacher, test_loader, criterion1, criterion2, device):
+    test_loss = 0.0
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+
+    model.eval()
+    for data, label in test_loader:
+        data = data.to(device=device, dtype=torch.float32)
+        label = label.to(device=device, dtype=torch.long)
+        with torch.no_grad():
+            output = model(data)
+            output_teacher = teacher(data)
+        loss = criterion1(output, label) + criterion2(output, output_teacher)
+        test_loss += loss.item() * data.size(0)
+        _, pred = torch.max(output, 1)
+        correct = np.squeeze(pred.eq(label.data.view_as(pred)))
+        for i in range(len(label)):
+            digit = label.data[i]
+            class_correct[digit] += correct[i].item()
+            class_total[digit] += 1
+
+    test_loss = test_loss / len(test_loader.sampler)
+    print('test Loss: {:.6f}\n'.format(test_loss))
+    for i in range(10):
+        print('test accuracy of %d: %2d%% (%2d/%2d)' % (
+        i, 100 * class_correct[i] / class_total[i], np.sum(class_correct[i]), np.sum(class_total[i])))
+    print('\ntest accuracy (overall): %2.2f%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total), np.sum(class_correct), np.sum(class_total)))
+    return (100. * np.sum(class_correct) / np.sum(class_total))
