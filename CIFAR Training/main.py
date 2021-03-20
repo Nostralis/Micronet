ae730f585e33a9ba5fcc652c9460f938ae8258d4
Blu3s1one <74374450+Blu3s1one@users.noreply.github.com>
2021-03-20 14:10:07 +0100

    changed for 2,2

--- CIFAR Training/main.py
+++ CIFAR Training/main.py
@@ -8,7 +8,7 @@ import time
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device ' + str(device))
 
-student_ = [1,1]
+student_ = [2, 2]
 teacher_ = [2, 2, 2, 2]
 
 student = ResNet18(student_)
